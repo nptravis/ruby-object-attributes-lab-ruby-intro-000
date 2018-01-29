5a8729f9d1be8ec155ebd699e353c897f5128329
Nic Travis <nptravis@yahoo.com>
2018-01-29 11:47:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
 
   def initialize(name)
-    @this_dogs_name = name
+    @name = name
   end
 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
 
   def name
-    @this_dogs_name
+    @name
   end
 
 end
